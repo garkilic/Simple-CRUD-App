23fff8e851a929140f07bcef4176be0b327a5603
Griffin Arkilic <garkilic@gmail.com>
2022-06-03 10:15:03 -0700

    Initial creation of CRUD App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<label>Name:</label>
+				<input type="text" />
+				<label>Age:</label>
+				<input type="number" />
+				<label>Country:</label>
+				<input type="text" />
+				<label>Salary:</label>
+				<input type="number" />
+				<label>Position:</label>
+				<input type="text" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
